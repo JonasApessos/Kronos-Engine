c75ca3c4cd0753b7123544ce1bbdc12a6d068ecc
JonasApessos <JonasApessos@gmail.com>
2023-02-02 22:19:43 +0200

    Added new primitives for testing Added a destructor for the shader from RAII implementation

--- Source/Shader.cpp
+++ Source/Shader.cpp
@@ -43,7 +43,15 @@ Shader::Shader(const GLchar* IncVertPath, const GLchar* IncFragPath)
     CreateShaderProg(iVertShaderRef, iFragShaderRef);
 }
 
-void Shader::CompileShader(GLuint& IniShaderRef, const GLchar* IncShaderCode, ShaderCompilationType InFShaderType)
+Shader::~Shader()
+{
+    if (glIsShader(ShaderProgID))
+    {
+
+    }
+}
+
+void Shader::CompileShader(GLuint& IniShaderID, const GLchar* IncShaderCode, ShaderCompilationType InFShaderType)
 {
     GLint iSuccess;
     GLchar cInfoLog[512];
@@ -51,45 +59,45 @@ void Shader::CompileShader(GLuint& IniShaderRef, const GLchar* IncShaderCode, Sh
     switch (InFShaderType)
     {
         case ShaderCompilationType::VertexShader:
-            IniShaderRef = glCreateShader(GL_VERTEX_SHADER);
+            IniShaderID = glCreateShader(GL_VERTEX_SHADER);
             break;
 
         case ShaderCompilationType::FragmentShader:
-            IniShaderRef = glCreateShader(GL_FRAGMENT_SHADER);
+            IniShaderID = glCreateShader(GL_FRAGMENT_SHADER);
             break;
     }
     
-    glShaderSource(IniShaderRef, 1, &IncShaderCode, NULL);
-    glCompileShader(IniShaderRef);
+    glShaderSource(IniShaderID, 1, &IncShaderCode, NULL);
+    glCompileShader(IniShaderID);
 
-    glGetShaderiv(IniShaderRef, GL_COMPILE_STATUS, &iSuccess);
+    glGetShaderiv(IniShaderID, GL_COMPILE_STATUS, &iSuccess);
 
     if (!iSuccess)
     {
-        glGetShaderInfoLog(IniShaderRef, 512, NULL, cInfoLog);
+        glGetShaderInfoLog(IniShaderID, 512, NULL, cInfoLog);
         cout << "FILE::" << sShaderFile << endl;
         cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << cInfoLog << endl;
     }
 }
 
-void Shader::CreateShaderProg(GLuint& IniVertShaderRef, GLuint& IniFragShaderRef)
+void Shader::CreateShaderProg(GLuint& IniVertShaderID, GLuint& IniFragShaderID)
 {
     GLint iSuccess;
     GLchar cInfoLog[512];
 
-    ShaderProgRef = glCreateProgram();
-    glAttachShader(ShaderProgRef, IniVertShaderRef);
-    glAttachShader(ShaderProgRef, IniFragShaderRef);
-    glLinkProgram(ShaderProgRef);
+    ShaderProgID = glCreateProgram();
+    glAttachShader(ShaderProgID, IniVertShaderID);
+    glAttachShader(ShaderProgID, IniFragShaderID);
+    glLinkProgram(ShaderProgID);
 
-    glGetProgramiv(ShaderProgRef, GL_LINK_STATUS, &iSuccess);
+    glGetProgramiv(ShaderProgID, GL_LINK_STATUS, &iSuccess);
 
     if (!iSuccess)
     {
-        glGetProgramInfoLog(ShaderProgRef, 512, NULL, cInfoLog);
+        glGetProgramInfoLog(ShaderProgID, 512, NULL, cInfoLog);
         cout << "ERROR:SHADER::PROGRAM::LINKING_FAILED\n" << cInfoLog << endl;
     }
 
-    glDeleteShader(IniVertShaderRef);
-    glDeleteShader(IniFragShaderRef);
+    glDeleteShader(IniVertShaderID);
+    glDeleteShader(IniFragShaderID);
 }
